6e6d56b10778d1fd7ee1e49f794e5625379d464d
André Neves <andreneves3@gmail.com>
2021-03-30 22:03:22 -0300

    build: local enviroment config

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# cg-aframe
+# Jogo Asteroids com A-Frame
+
+## Execução para desenvolvimento
+
+### Dependências de ambiente
+
+```
+Node 14
+```
+
+
+### Instação das dependências internas
+
+```
+npm install
+```
+
+### Execução
+
+```
+npm run start
+```
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,29 @@
+
+const livereload = require('livereload');
+const connect  = require('connect');
+const compiler = require('connect-compiler');
+const static = require('serve-static');
+
+
+
+console.log('Start web livereload server...')
+
+
+const  server = connect();
+server.use(
+  compiler({
+    enabled : [ 'coffee', 'uglify' ],
+    src     : 'src',
+    dest    : 'public'
+  })
+);
+
+const port = 8080;
+
+server.use(  static(__dirname + '/public'));
+server.listen(port);
+
+const lrserver = livereload.createServer();
+lrserver.watch(__dirname + "/public");
+
+console.log(`Listening on port ${port}`);
