bd1c4dd6f72df11bba5490424b42ff925566dc46
Leonam <leonan.aiirrc02t@gmail.com>
2021-04-13 20:27:09 -0300

    feat: adding bullet component

---
+++ public/studies/tests-1/animation/bullet.js
@@ -0,0 +1,44 @@
+function registerBulletMoveComponent() {
+    AFRAME.registerComponent("bullet", {
+        schema: {
+          velocityX: { type: "number", default: 10 },
+          velocityY: { type: "number", default: 0 },
+          velocityZ: { type: "number", default: 10 },
+          directionX: { type: "number", default: 10 },
+          directionZ: { type: "number", default: 10 },
+          limit : { type: "number", default: 500 }
+        },
+        init: function () {
+          const scene = document.querySelector("a-scene");
+          this.initialTime = scene.time;
+          this.initialX = this.el.getAttribute("position").x;
+          this.initialY = this.el.getAttribute("position").y;
+          this.initialZ = this.el.getAttribute("position").z;
+        },
+    
+        update: function () {},
+    
+        remove: function () {},
+    
+        tick: function (time) {
+            const componentTime = time - this.initialTime;
+        
+            const angularCoefficient = ( this.data.directionZ - this.initialZ ) / ( this.data.directionX - this.initialX )
+            const angleMovement = Math.atan ( angularCoefficient )
+
+            const x = this.initialX + (componentTime * Math.cos( angleMovement )) / 10;
+            const y = this.initialY + (componentTime * this.data.velocityY) / 10;
+            const z = this.initialZ + (componentTime * Math.sin( angleMovement )) / 10;
+        
+            this.el.setAttribute("position", {
+                x,
+                y,
+                z,
+            });
+        
+            if (x > this.data.limit || z > this.data.limit || x < ( -this.data.limit ) || z < ( -this.data.limit )) {
+              this.el.parentNode.removeChild(this.el);
+            }
+        },
+      });
+}
